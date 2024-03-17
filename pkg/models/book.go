5a3647ab272ab7d2d0a3fe37ee5d08d1a81bbc16
Roy Lopez <roylopezdev@gmail.com>
2024-03-17 20:30:30 -0300

    Refactor Book model and add CRUD operations

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -2,14 +2,14 @@ package models
 
 import (
 	"github.com/Thebassplayer/go_book_management_system/pkg/config"
-	"github.com/jinzhu/gorm"
+	"gorm.io/gorm"
 )
 
 var db *gorm.DB
 
 type Book struct {
 	gorm.Model
-	Name        string `gorm:""json:"name"`
+	Name        string `gorm:"" json:"name"`
 	Author      string `json:"author"`
 	Publication string `json:"publication"`
 }
@@ -18,4 +18,45 @@ func init() {
 	config.Connect()
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book {
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(ID int64) Book {
+	var book Book
+	db.Where("id=?", ID).Delete(book)
+	return book
+}
+
+func UpdateBook(ID int64, updatedBook *Book) (*Book, *gorm.DB) {
+	var book Book
+	db = db.Where("ID = ?", ID).Find(&book)
+
+	if db.Error != nil {
+		// Handle error, book not found
+		return nil, db
+	}
+
+	db = db.Model(&book).Updates(Book{
+		Name:        updatedBook.Name,
+		Author:      updatedBook.Author,
+		Publication: updatedBook.Publication,
+	})
+
+	return &book, db
 }
