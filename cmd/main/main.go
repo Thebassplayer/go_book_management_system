0d69ae219635de984411f7f270f147a0ef8ec81d
Roy Lopez <roylopezdev@gmail.com>
2024-03-17 20:30:02 -0300

    Add logging middleware and update main route handler

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/Thebassplayer/go_book_management_system/pkg/middleware"
 	"github.com/Thebassplayer/go_book_management_system/pkg/routes"
 	"github.com/gorilla/mux"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func main() {
 	r := mux.NewRouter()
+	r.Use(middleware.LoggingMiddleware)
+	// Print the path hit
+	r.HandleFunc("/",
+		func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Welcome to the Go Book Management System!")
+		},
+	).Methods("GET")
+	// Import the RegisterBookStoreRoutes function from the routes package and call it with the router object
 	routes.RegisterBookStoreRoutes(r)
-	http.Handle("/", r)
-	log.Fatal(http.ListenAndServe("localhost:9010", r))
+
+	fmt.Printf("Starting server at port 9010\n")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
